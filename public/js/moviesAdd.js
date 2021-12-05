39f6020b077b1b2dea0d0ff5b995c7135155e3b4
DávalosLeonardo <davaloslm@hotmail.com.ar>
2021-12-04 21:28:50 -0300

    Completa micro desafio 3

---
+++ public/js/moviesAdd.js
@@ -0,0 +1,15 @@
+/* Micro desafío - Paso 3: */
+
+var h1 = document.querySelector("h1");
+var section = document.querySelector("section");
+var article = document.querySelector("article");
+
+/* ///h1/// */
+h1.innerText += "AGREGAR PELÍCULAS";
+h1.classList.add("titulo");
+
+/* ///article/// */
+article.classList.add("fondoTransparente");
+
+/* ///section/// */
+section.classList.add("fondoCRUD");
